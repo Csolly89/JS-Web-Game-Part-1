81eacac66125ca0d69a39593710a5a8a723d65e8
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-08-29 20:24:50 -0400

    drawImg fucntion set

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function drawImg(asset) {
+    let image = document.createElement('img')
+    image.src = asset
+    image.style.position = 'fixed'
+    image.style.left = '100px'
+    image.style.bottom = '100px'
+    document.body.append(image)
+}
+
+
+drawImg("assets/green-character.gif")
+drawImg("assets/pine-tree.png")
