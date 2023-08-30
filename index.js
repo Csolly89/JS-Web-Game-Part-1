b31860e2ae2c83ca3fee93e5e27fe0c96a70f513
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-08-30 17:32:52 -0400

    functions and event listeners added

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-function drawImg(asset) {
+function drawImg(asset, left, bottom) {
     let image = document.createElement('img')
     image.src = asset
     image.style.position = 'fixed'
-    image.style.left = '100px'
-    image.style.bottom = '100px'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom +'px'
     document.body.append(image)
 }
 
+// trying to make new function specific for even listener items
 
-drawImg("assets/green-character.gif")
-drawImg("assets/pine-tree.png")
+// function useItem(asset, left, bottom){
+// let image = document.createElement('img')
+// image.src = asset
+// image.position = 'fixed'
+// image.left = left + "px"
+// image.bottom = bottom = "px"
+// document.body.append(image)
+
+// asset.addEventListener('dblclick', function(){
+//     asset.remove()
+// })
+
+
+
+
+drawImg("assets/green-character.gif", 100, 100)
+drawImg("assets/pine-tree.png", 450, 400) 
+drawImg("assets/tree.png", 200, 350)
+drawImg("assets/pillar.png", 350, 100)
+drawImg("assets/well.png", 500, 725)
+drawImg("assets/crate.png", 150, 200)
+
+drawImg("assets/shield.png", 165, 185)
+drawImg("assets/staff.png", 600, 100)
+
+// useItem("assets/sword.png", 500, 105)
+
+let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('dblclick', function(){
+    sword.remove()
+})
